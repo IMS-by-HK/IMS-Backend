ff9eebb9412760c9486502fe3f6107987e933e2e
Kate G <161684914+k8-g@users.noreply.github.com>
2024-12-13 15:44:54 +1000

    Initialising app
    - basic set up
    - index.js
    - server.js
    - basic run dev script command
    - readme updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # IMS-Backend
+
+Command to run server:/
+`npm run dev`
+
+---
+Initalising/dependencies installed:
+- `npm init -y`
+- `npm install mongoose express`
+- `npm install --save-dev nodemon`
---
+++ src/server.js
@@ -0,0 +1,35 @@
+// Purpose:
+// Configure the server, eg.
+// - routes
+// - middleware 
+// - CORS 
+// - debug logger setups
+// - connections to databases
+// - connections to file storage 
+
+const express = require("express");
+const app = express();
+
+// Server app configuration goes here
+// middleware, routes, etc 
+
+app.use(express.json());
+
+// app.verb(path, callback);
+app.get("/", (request, response) => {
+	// response.send("<h1>Hello, world!</h1>");
+
+    response.json({
+		message:"Hello world!"
+	});
+});
+
+// Import router and tell app to use router
+// const ProductController = require("./controllers/ProductController.js");
+// app.use("/products", ProductController);
+
+// Server app configuration is finished by this point 
+// Export the app so that other files can control when the server starts and stops 
+module.exports = {
+	app
+}
